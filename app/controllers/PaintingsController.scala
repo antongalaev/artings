33001ada42249752aa2f09b2698dd61983cea07f
antongalaev <wgalaev@gmail.com>
2024-09-29 18:29:51 +0200

    show paintings in full

--- app/controllers/PaintingsController.scala
+++ app/controllers/PaintingsController.scala
@@ -19,4 +19,14 @@ class PaintingsController @Inject()(cc: ControllerComponents, counter: Counter)
     val count = counter.nextCount().toString
     Ok(views.html.paintings("Paintings!"))
   }
+
+  def painting(name: String) = Action {
+    val count = counter.nextCount().toString
+    val title = name.substring(0, name.lastIndexOf("."))
+      .split("_")
+      .map(_.capitalize)
+      .mkString(" ")
+
+    Ok(views.html.painting(title, name))
+  }
 }
