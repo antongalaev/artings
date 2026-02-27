1f3e9c0ca8cf917139c82c7edf618faaeaa48ca0
antongalaev <wgalaev@gmail.com>
2026-02-27 12:19:35 +0100

    update title and contacts

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -12,6 +12,6 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Anton Galaev Artings"))
+    Ok(views.html.index("Anton Galaev"))
   }
 }
